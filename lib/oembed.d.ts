fa6bf2a3a127c6e3d61a8c81cad6bafa3d8d4cf4
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-11-25 19:16:27 +0000

    Adding TypeScript definition.

---
+++ lib/oembed.d.ts
@@ -0,0 +1,26 @@
+// Type definitions for node-oembed (aendrew's fork)
+// Project: https://github.com/aendrew/node-oembed
+// Definitions by: Ændrew Rininsland <https://github.com/aendrew>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module "oembed" {
+  import request = require("superagent");
+
+  type CallbackHandler = { (err: any, res: request.Response): void; }|{ (res: request.Response): void; };
+
+  function fetch(url:string, parameters:IOembedOptions, cb:CallbackHandler):void;
+
+  function discover(url:string, cb:CallbackHandler):void;
+
+  function fetchJSON(url:string, cb:CallbackHandler):void;
+
+  function fetchXML(url:string, cb:CallbackHandler):void;
+
+  var EMBEDLY_KEY:string;
+
+  interface IOembedOptions {
+    key?: string;
+    url?: string;
+    format?: string;
+  }
+}
