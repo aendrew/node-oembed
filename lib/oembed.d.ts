5d0616d3ea3630dada3316a5393abcf24389e0b3
Ændrew Rininsland <aendrew.rininsland@the-times.co.uk>
2015-11-25 19:42:58 +0000

    Don't need no stinkin' ambient declarations

--- lib/oembed.d.ts
+++ lib/oembed.d.ts
@@ -3,28 +3,26 @@
 // Definitions by: Ændrew Rininsland <https://github.com/aendrew>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path='../typings/superagent/superagent.d.ts' />
+import request = require("superagent");
 
-declare module "oembed" {
-  import request = require("superagent");
-
+declare module oembed {
   type CallbackHandler = { (err: any, res: request.Response): void; }|{ (res: request.Response): void; };
 
-  export module oembed {
-    function fetch(url:string, parameters:IOembedOptions, cb:CallbackHandler):void;
+  function fetch(url:string, parameters:IOembedOptions, cb:CallbackHandler):void;
 
-    function discover(url:string, cb:CallbackHandler):void;
+  function discover(url:string, cb:CallbackHandler):void;
 
-    function fetchJSON(url:string, cb:CallbackHandler):void;
+  function fetchJSON(url:string, cb:CallbackHandler):void;
 
-    function fetchXML(url:string, cb:CallbackHandler):void;
+  function fetchXML(url:string, cb:CallbackHandler):void;
 
-    var EMBEDLY_KEY:string;
+  var EMBEDLY_KEY:string;
 
-    interface IOembedOptions {
-      key?: string;
-      url?: string;
-      format?: string;
-    }
+  interface IOembedOptions {
+    key?: string;
+    url?: string;
+    format?: string;
   }
 }
+
+export = oembed;
